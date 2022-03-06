dc8660ab964e8d1b32b586b34c11fac3ee3e8c9d
Krystyna Szybalska <Krystyna.Szybalska@microsoft.wsei.edu.pl>
2022-03-06 08:46:01 +0100

    completed to: creating the RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks {
+    class RankedGradeBook : BaseGradeBook {
+        public RankedGradeBook(string name) : base(name) {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade) {
+            return base.GetLetterGrade(averageGrade);
+        }
+    }
+}
